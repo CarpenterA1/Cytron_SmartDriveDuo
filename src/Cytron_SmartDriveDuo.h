766bfb088592a1749310af0b8a2358dbf69b2232
CarpenterA1 <114365656+CarpenterA1@users.noreply.github.com>
2024-04-18 12:11:16 -0400

    Update Cytron_SmartDriveDuo.h

--- src/Cytron_SmartDriveDuo.h
+++ src/Cytron_SmartDriveDuo.h
@@ -3,9 +3,12 @@
 
 #include "Arduino.h"
 
-
-#define Serial Serial1
-
+// Arduino Leonardo
+#if defined (__AVR_ATmega32U4__)
+  #define Serial Serial1
+#else
+  #define Serial Serial
+#endif
 
 enum {
   RC_MCU,
@@ -18,19 +21,17 @@ enum {
 class Cytron_SmartDriveDuo
 {
   public:
-  	//Cytron_SmartDriveDuo(int mode, int rc1Pin, int rc2Pin); // For RC MCU mode
     Cytron_SmartDriveDuo(int mode, int in1Pin, int in2Pin, int an1Pin, int an2Pin); // For PWM Independent mode
-    Cytron_SmartDriveDuo(int mode, int txPin, uint32_t baudrate); // For Serial Simplified mode
-    Cytron_SmartDriveDuo(int mode, int txPin, int boardId, uint32_t baudrate); // For Serial Packetized mode
+    Cytron_SmartDriveDuo(int mode, uint32_t baudrate); // For Serial Simplified mode
+    Cytron_SmartDriveDuo(int mode, int boardId, uint32_t baudrate); // For Serial Packetized mode
     void initialByte(uint8_t dummyByte);
     void control(signed int motorLeftSpeed, signed int motorRightSpeed);
     
   private:
-    
   	uint8_t _mode;
   	uint8_t _rc1Pin, _rc2Pin;
   	uint8_t _an1Pin, _an2Pin, _in1Pin, _in2Pin;
-  	uint8_t _txPin, _rxPin = 99, _boardId;
+  	uint8_t _boardId;
     uint8_t headerByte = 0x85, addressByte, commandByte, checksum;
     int _motorLSpeed, _motorRSpeed;
     int rc1Delay, rc2Delay;
